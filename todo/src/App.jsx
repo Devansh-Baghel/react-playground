e66ddc663cf6063306d4890f48ce44c839ce6e57
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-08-27 19:36:03 +0530

    adding tasks works

---
+++ todo/src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+function App() {
+  const [todo, setTodo] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleChange(event) {
+    setNewTask(event.target.value);
+  }
+
+  function addTask() {
+    setTodo([...todo, newTask]);
+  }
+
+  return (
+    <div className="app">
+      <div className="add-task">
+        <input onChange={handleChange} />
+        <button onClick={addTask}> Add Task</button>
+      </div>
+
+      <div className="list">
+        {todo.map(item => {
+          return <h2>{item}</h2>
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
