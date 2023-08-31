ef38417fee9c819c54ad6bfa42be47cfc6c383bf
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-08-31 20:56:04 +0530

    rename module6 to lifecycle

--- todo/src/main.jsx
+++ todo/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './module6/App'
+import App from './lifecycle/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
