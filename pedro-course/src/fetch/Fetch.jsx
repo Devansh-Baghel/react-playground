c8b4f2375b7ccc4be531e265d54f38495432bb29
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-09-01 17:38:49 +0530

    Button works now

--- pedro-course/src/fetch/Fetch.jsx
+++ pedro-course/src/fetch/Fetch.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import axios from "axios";
+import { useState } from "react";
 
 function Fetch() {
+  const [catFact, setCatFact] = useState("");
+
   function getFact() {
     axios.get("https://catfact.ninja/fact").then((res) => {
-      console.log(res.data)
-      return res.data
+      setCatFact(res.data.fact)
     });
   }
 
   return (
     <div>
       <button onClick={getFact}>Generate Cat Fact</button>
-      <p></p>
+      <p>{catFact}</p>
     </div>
   );
 }
