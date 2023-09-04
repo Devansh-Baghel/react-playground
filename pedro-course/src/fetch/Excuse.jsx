5736d75ec46bb87e225a7069d2fa8bc75ffb585c
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-09-04 20:19:55 +0530

    Add Excuse jsx

---
+++ pedro-course/src/fetch/Excuse.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Excuse() {
+  return (
+    <div>
+      <h3>Generate an Excuse</h3>
+      <button>Party</button>
+      <button>Family</button>
+      <button>Office</button>
+    </div>
+  )
+}
+
+export default Excuse
