0697a924191e53c193abb65d5b97ff8b1871e297
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-09-04 20:34:29 +0530

    Finished Excuse.jsx

--- pedro-course/src/fetch/Excuse.jsx
+++ pedro-course/src/fetch/Excuse.jsx
@@ -1,12 +1,23 @@
+import axios from 'axios';
 import React from 'react'
+import { useState } from 'react'
 
 function Excuse() {
+  const [display, setDisplay] = useState("...");
+
+  function getExcuse(category){
+    axios.get(`https://excuser-three.vercel.app/v1/excuse/${category}`).then((res) => {
+      setDisplay(res.data[0].excuse)
+    })
+  }
+
   return (
     <div>
-      <h3>Generate an Excuse</h3>
-      <button>Party</button>
-      <button>Family</button>
-      <button>Office</button>
+      <h2>Generate an Excuse</h2>
+      <button onClick={() => getExcuse("party")}>Party</button>
+      <button onClick={() => getExcuse("family")}>Family</button>
+      <button onClick={() => getExcuse("office")}>Office</button>
+      <h5>Excuse: {display}</h5>
     </div>
   )
 }
--- pedro-course/src/main.jsx
+++ pedro-course/src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import Todo from "./todo/Todo"
 import Fetch from './fetch/Fetch'
 import Age from './fetch/Age'
+import Excuse from './fetch/Excuse'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Age />
+    <Excuse />
   </React.StrictMode>,
 )
