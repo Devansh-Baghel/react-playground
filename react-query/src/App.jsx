b901eee29c8b68fdbce0d118e5520474ab5738b3
Devansh Baghel <devanshbaghel85@gmail.com>
2023-11-27 20:55:38 +0530

    useMutation

--- react-query/src/App.jsx
+++ react-query/src/App.jsx
@@ -1,9 +1,6 @@
 import React from "react";
-import {
-  ReactQueryDevtools,
-} from "@tanstack/react-query-devtools";
 
-import { useQuery, useMutation } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 
 const POSTS = [
   { id: 1, title: "Post 1" },
@@ -11,10 +8,22 @@ const POSTS = [
 ];
 
 function App() {
+  const queryClient = useQueryClient();
   const postsQuery = useQuery({
     queryKey: ["posts"],
     queryFn: () => wait(1000).then(() => [...POSTS]),
-    // queryFn: () => Promise.reject("random error message"),
+  });
+
+  const newPostMutation = useMutation({
+    mutationFn: (title) => {
+      return wait(1000).then(() => {
+        POSTS.push({ id: crypto.randomUUID(), title });
+      });
+    },
+
+    onSuccess: () => {
+      queryClient.invalidateQueries(["posts"]);
+    },
   });
 
   if (postsQuery.isLoading) return <h1>Loading... </h1>;
@@ -24,10 +33,17 @@ function App() {
   }
 
   return (
-    <>
-      <ReactQueryDevtools />
-      <div>App</div>
-    </>
+    <div>
+      {postsQuery.data?.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+      <button
+        disabled={newPostMutation.isPending}
+        onClick={() => newPostMutation.mutate("New Post")}
+      >
+        Add New Post
+      </button>
+    </div>
   );
 }
 
