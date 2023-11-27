9d75f85c56f15429e5a3d864934936924e43aec9
Devansh Baghel <devanshbaghel85@gmail.com>
2023-11-27 17:31:06 +0530

    added react query dev tools, useQuery

--- react-query/src/App.jsx
+++ react-query/src/App.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import {
+  ReactQueryDevtools,
+} from "@tanstack/react-query-devtools";
+
+import { useQuery, useMutation } from "@tanstack/react-query";
+
+const POSTS = [
+  { id: 1, title: "Post 1" },
+  { id: 2, title: "Post 2" },
+];
 
 function App() {
+  const postsQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => wait(1000).then(() => [...POSTS]),
+    // queryFn: () => Promise.reject("random error message"),
+  });
+
+  if (postsQuery.isLoading) return <h1>Loading... </h1>;
+
+  if (postsQuery.isError) {
+    return <pre>{JSON.stringify(postsQuery.error)}</pre>;
+  }
+
   return (
-    <div>App</div>
-  )
+    <>
+      <ReactQueryDevtools />
+      <div>App</div>
+    </>
+  );
 }
 
-export default App
+function wait(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+export default App;
